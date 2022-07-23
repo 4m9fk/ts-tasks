11c47dd830e591758a184a2027dd1c9c93a93385
4m9fka <4m9fka@gmail.com>
2022-07-23 21:58:06 +0300

    Init

---
+++ tasks/math/addition/indes.test.ts
@@ -0,0 +1,7 @@
+import { addtion } from "./index";
+
+describe("addition", () => {
+  it("should return 5", () => {
+    expect(addtion(2, 3)).toBe(5);
+  });
+});
---
+++ tasks/math/addition/index.ts
@@ -0,0 +1,1 @@
+export const addtion = (a: number, b: number): number => a + b;
