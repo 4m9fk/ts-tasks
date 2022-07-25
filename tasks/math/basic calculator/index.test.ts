0cd133dc515b01b73df7f3c7fc9c81f17ca76e6e
v.volkova <v.volkova@godeltech.com>
2022-07-25 16:27:02 +0300

    Create basic calculator, test and description file for it

---
+++ tasks/math/basic calculator/index.test.ts
@@ -0,0 +1,13 @@
+import { calculator } from "./index";
+
+describe("calculator", () => {
+  it("should return 4", () => {
+    expect(calculator(2, "+", 2)).toBe(4);
+  });
+  it("should return 4", () => {
+    expect(calculator(2, "*", 2)).toBe(4);
+  });
+  it("should return 2", () => {
+    expect(calculator(4, "/", 2)).toBe(2);
+  });
+});
