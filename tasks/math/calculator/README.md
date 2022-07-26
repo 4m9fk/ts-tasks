69141153cd40945b060282f5dd109910bc613d8e
v.volkova <v.volkova@godeltech.com>
2022-07-26 15:06:24 +0300

    Solve prettier problems, update type for operator variable

--- tasks/math/calculator/README.md
+++ tasks/math/calculator/README.md
@@ -1,11 +1,11 @@
 # Basic Calculator
 
-Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+Create a function that takes two numbers and a mathematical operator + - / \* and will perform a calculation with the given numbers.
 
 ### Examples
 
->calculator(2, "+", 2) ➞ 4
+> calculator(2, "+", 2) ➞ 4
 
->calculator(2, "*", 2) ➞ 4
+> calculator(2, "\*", 2) ➞ 4
 
->calculator(4, "/", 2) ➞ 2
+> calculator(4, "/", 2) ➞ 2
--- tasks/math/calculator/index.test.ts
+++ tasks/math/calculator/index.test.ts
@@ -1,13 +1,13 @@
-import { calculator } from "./index";
+import { calculator } from './index';
 
-describe("calculator", () => {
-  it("should return 4", () => {
-    expect(calculator(2, "+", 2)).toBe(4);
+describe('calculator', () => {
+  it('should return 4', () => {
+    expect(calculator(2, '+', 2)).toBe(4);
   });
-  it("should return 4", () => {
-    expect(calculator(2, "*", 2)).toBe(4);
+  it('should return 4', () => {
+    expect(calculator(2, '*', 2)).toBe(4);
   });
-  it("should return 2", () => {
-    expect(calculator(4, "/", 2)).toBe(2);
+  it('should return 2', () => {
+    expect(calculator(4, '/', 2)).toBe(2);
   });
 });
