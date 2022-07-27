7322835ce739eef042e8bc692d97034ab6e56c07
v.volkova <v.volkova@godeltech.com>
2022-07-27 15:53:21 +0300

    Solve Product of two prime numbers task

--- tasks/math/productOfPrimes/index.test.ts
+++ tasks/math/productOfPrimes/index.test.ts
@@ -1,10 +1,9 @@
 import { productOfPrimes } from "./index";
-
 describe("productOfPrimes", () => {
-    it ("should return true", () => {
-        expect(productOfPrimes(25).valueOf());
-    })
-    it ("should return false", () => {
-        expect(productOfPrimes(10).valueOf());
-    })
-})
+  it("should return true", () => {
+    expect(productOfPrimes(25).valueOf());
+  });
+  it("should return false", () => {
+    expect(productOfPrimes(10).valueOf());
+  });
+});
--- tasks/math/productOfPrimes/index.ts
+++ tasks/math/productOfPrimes/index.ts
@@ -1,18 +1,16 @@
-export const productOfPrimes = function (value: number):boolean {
-    for (let i= 1; i<value; i++ ) {
-        if ( isPrime(i) && value%i==0 && isPrime(value/i) )
-			return true;
-	else return false;
-    }
-}
+export const productOfPrimes = function (value: number): boolean {
+  for (let i = 1; i < value; i++) {
+    if (isPrime(i) && value % i == 0 && isPrime(value / i)) return true;
+  }
+  return false;
+};
 
-const isPrime = (num:number) => {
-    for (var i = 2; i < num; i++)
-	{
-		if (num%i == 0) return false;
-	}
-	return true;
-}
+const isPrime = (num: number): boolean => {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
+};
 
 console.log(productOfPrimes(12));
 console.log(productOfPrimes(25));
