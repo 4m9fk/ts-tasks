acdf5831fcdc06682f2828188312977f1286270d
Nataliia Samofalova <n.samofalova@godeltech.com>
2022-07-28 10:34:07 +0200

    Letter Occurrences Per Word

---
+++ tasks/letterOccurrences/index.ts
@@ -0,0 +1,26 @@
+// function findOccurrences(str, char) {
+// 	return str
+// 		.toLowerCase()
+// 		.split(" ")
+// 		.reduce((acc, word) => {
+// 			return {
+// 				...acc,
+// 				[word]: [...word].filter(el => el === char.toLowerCase()).length
+// 			}
+// 		}, {})
+// }
+
+export const findOccurrences = (
+  str: string,
+  char: string
+): { [key: string]: number } => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .reduce((acc, word) => {
+      return {
+        ...acc,
+        [word]: [...word].filter((el) => el === char.toLowerCase()).length,
+      };
+    }, {});
+};
