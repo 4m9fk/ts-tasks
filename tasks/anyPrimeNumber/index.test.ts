c5e10f8af692a0440a24c7ab2a3374a5760c6557
Nataliia Samofalova <n.samofalova@godeltech.com>
2022-08-03 15:57:13 +0200

    Bugs fixed

--- tasks/anyPrimeNumber/index.test.ts
+++ tasks/anyPrimeNumber/index.test.ts
@@ -2,18 +2,18 @@ import { primeInRange } from "./index";
 
 describe("primeInRange", () => {
   it(" Prime numbers 10, 15", () => {
-    expect(primeInRange(10, 15)).toBe(true);
+    expect(primeInRange(10, 15)).toBeTruthy();
   });
 });
 
 describe("primeInRange", () => {
   it("Prime numbers 62, 66", () => {
-    expect(primeInRange(62, 66)).toBe(false);
+    expect(primeInRange(62, 66)).toBeFalsy();
   });
 });
 
 describe("primeInRange", () => {
   it("Prime numbers 3, 5", () => {
-    expect(primeInRange(3, 5)).toBe(true);
+    expect(primeInRange(3, 5)).toBeTruthy();
   });
 });
